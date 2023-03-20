99a51e6d926328476042d1fd0fc0ee60a6ab7af6
taivoit1993 <taivo.mile@gmail.com>
2023-03-20 17:02:21 +0700

    update

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Calculator;
+use src\GetStart\Calculator as GetStartCalculator;
 
 class CalculatorTest extends \PHPUnit\Framework\TestCase
 {
@@ -35,6 +36,15 @@ class CalculatorTest extends \PHPUnit\Framework\TestCase
         $result = $this->calculator->subtract(20, 5);
 
         $this->assertEquals(15, $result);
+    }
+
+    public function testWithStub(){
+        $calculator = $this->getMockBuilder(Calculator::class)->getMock();
+        $calculator->expects($this->any())
+        ->method('add')
+        ->will($this->returnValue(6));
+
+        $this->assertEquals(6, $calculator->add(100,100));
     }
 
     public function tearDown(): void
